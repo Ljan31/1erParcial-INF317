fd2abd1756d7c88571999f5dbb491dd572321be0
Ljan31 <108371723+Ljan31@users.noreply.github.com>
2024-10-10 23:38:29 -0400

    Add files via upload

---
+++ 7/Calculadora/Calculadora/ClassEvaluar.cs
@@ -0,0 +1,99 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Text.RegularExpressions;
+namespace Calculadora
+{
+    class ClassEvaluar
+    {
+        public double EvaluarExpresion(string expresion)
+        {
+            if (!EsValida(expresion))
+                throw new ArgumentException("La expresión no es válida.");
+
+            return Evaluar(expresion);
+        }
+
+        private bool EsValida(string expresion)
+        {
+            // Expresión regular para validar la cadena
+            string patron = @"^\d+(\s*[\+\-\*\/]\s*\d+)*$";
+            // Comprueba si la expresión contiene operadores consecutivos o está mal formada
+            return Regex.IsMatch(expresion, patron);
+        }
+
+        private double Evaluar(string expresion)
+        {
+            Stack<double> operandos = new Stack<double>();
+            Stack<char> operadores = new Stack<char>();
+
+            for (int i = 0; i < expresion.Length; i++)
+            {
+                char c = expresion[i];
+
+                if (char.IsDigit(c))
+                {
+                    // Leer el número completo
+                    StringBuilder sb = new StringBuilder();
+                    while (i < expresion.Length && char.IsDigit(expresion[i]))
+                    {
+                        sb.Append(expresion[i]);
+                        i++;
+                    }
+                    i--; // Ajustar el índice después de salir del bucle
+                    operandos.Push(double.Parse(sb.ToString()));
+                }
+                else if (c == '+' || c == '-' || c == '*' || c == '/')
+                {
+                    while (operadores.Count > 0 && Prioridad(c) <= Prioridad(operadores.Peek()))
+                    {
+                        double b = operandos.Pop();
+                        double a = operandos.Pop();
+                        char op = operadores.Pop();
+                        operandos.Push(EjecutarOperacion(a, b, op));
+                    }
+                    operadores.Push(c);
+                }
+            }
+
+            while (operadores.Count > 0)
+            {
+                double b = operandos.Pop();
+                double a = operandos.Pop();
+                char op = operadores.Pop();
+                operandos.Push(EjecutarOperacion(a, b, op));
+            }
+
+            return operandos.Pop();
+        }
+
+        private int Prioridad(char op)
+        {
+            switch (op)
+            {
+                case '+':
+                case '-':
+                    return 1;
+                case '*':
+                case '/':
+                    return 2;
+                default:
+                    return 0;
+            }
+        }
+
+        private double EjecutarOperacion(double a, double b, char op)
+        {
+            switch (op)
+            {
+                case '+': return a + b;
+                case '-': return a - b;
+                case '*': return a * b;
+                case '/': return a / b;
+                default: throw new InvalidOperationException("Operador no válido.");
+            }
+        }
+    }
+}
---
+++ 7/Calculadora/Calculadora/Form1.cs
@@ -0,0 +1,72 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace Calculadora
+{
+    public partial class Form1 : Form
+    {
+        String n;
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+    
+        private void composicion(object sender, EventArgs e)
+        {
+            //n = n * 10 + Convert.ToInt32( ((Button)sender).Text );
+
+            n = n + (((Button)sender).Text);
+            textBox1.Text = n;
+        }
+
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+            n = "";
+            textBox1.Text = n;
+        }
+
+        private void Form1_Load_1(object sender, EventArgs e)
+        {
+
+        }
+
+        private void button15_Click(object sender, EventArgs e)
+        {
+            ClassEvaluar ev = new ClassEvaluar();
+            try
+            {
+                double resultado = ev.EvaluarExpresion(textBox1.Text);
+                label1.Text = "Resultado: " + resultado.ToString();
+            }
+            catch (Exception ex)
+            {
+                label1.Text = "Error: " + ex.Message;
+            }
+        }
+
+        private void button16_Click(object sender, EventArgs e)
+        {
+            textBox1.Text = "";
+            label1.Text = "";
+            //n = "";
+        }
+
+        private void button17_Click(object sender, EventArgs e)
+        {
+            if (n.Length > 0) // Asegúrate de que la cadena no esté vacía
+            {
+                n = n.Substring(0, n.Length - 1); // Elimina el último carácter
+                textBox1.Text = n; // Actualiza el TextBox con la nueva cadena
+            }
+        }
+    }
+}
